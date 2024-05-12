130771081d54b2f2c490f24be71c788b9db21716
drutitati6 <90923056+drutitati6@users.noreply.github.com>
2024-05-13 02:20:51 +0530

    index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({
     extended:true
 }))
-mongoose.connect('mongodb+srv://codedipa:pygHqt8jjzo30M5s@druti.suehx0x.mongodb.net/?retryWrites=true&w=majority&appName=druti')
+mongoose.connect('mongodb+srv://codedipa:*********@druti.suehj0x.mongodb.net/?retryWrites=true&w=majority&appName=druti')
 var db=mongoose.connection
 db.on('error',()=> console.log("Error in Connecting to Database"))
 db.once('open', () => console.log('Connected to Database'));
